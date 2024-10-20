9acad3d69724e76ac6b1e76dbc0dc9a5e4b39273
verystellar <161178496+verystellar@users.noreply.github.com>
2024-10-20 16:08:17 -0500

    implemented triangle calculator

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,15 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+   int value(int a){
+       if (a == 1) return 1;
+       return (a + value(a-1));
+   }
+
+   int add(int a, int b){
+       return value(a) + value(b);
+   }
+    int subtract(int a, int b){
+        return value(a) - value(b);
+    }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,8 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -11,5 +13,40 @@ public class TestTriangleNumberCalculator {
     calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  void test1() {
+    assertEquals(1, calculator.value(1));
+  }
+  @Test
+  void test2() {
+    assertEquals(3, calculator.value(2));
+  }
+  @Test
+  void test3() {
+    assertEquals(10, calculator.value(4));
+  }
+  @Test
+  void test4() {
+    assertEquals(2, calculator.add(1,1));
+  }
+  @Test
+  void test5() {
+    assertEquals(9, calculator.add(2,3));
+  }
+  @Test
+  void test6() {
+    assertEquals(13, calculator.add(2,4));
+  }
+  @Test
+  void test7() {
+    assertEquals(0, calculator.subtract(1,1));
+  }
+  @Test
+  void test8() {
+    assertEquals(-3, calculator.subtract(2,3));
+  }
+  @Test
+  void test9() {
+    assertEquals(7, calculator.subtract(4,2));
+  }
 }
